30b00c602214352a6d55054a2710791db05922fa
Unnikrishnan <unnikottamam@gmail.com>
2021-04-11 17:06:47 -0700

    working on the woocommerce loops

--- wp-content/themes/haussausageco/woocommerce/archive-product.php
+++ wp-content/themes/haussausageco/woocommerce/archive-product.php
@@ -18,15 +18,6 @@
 defined('ABSPATH') || exit();
 
 get_header('shop');
-
-/**
- * Hook: woocommerce_before_main_content.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- * @hooked WC_Structured_Data::generate_website_data() - 30
- */
-do_action('woocommerce_before_main_content');
 ?>
 <section class="banner text-primary">
    <div class="container">
@@ -44,85 +35,92 @@ do_action('woocommerce_before_main_content');
    </div>
 </section>
 <section id="featured-products" class="carousel slide featureprods" data-ride="carousel">
-   <div class="carousel-inner">
+   <div class="carousel-inner text-center text-md-right">
       <div class="carousel-item active">
-         <img src="images/" alt="featured-product">
+         <img src="<?php echo get_template_directory_uri(); ?>/images/featured-thumbnail.jpg" alt="featured-product">
          <div class="container">
             <div class="row justify-content-end">
                <div class="col-md-6">
-                  <h3>Featured</h3>
-                  <h2>maple blueberry</h2>
-                  <p>The ultimate breakfast sausage. %100 maple syrup with allspice and marjoram, and of course, lots of
-                     fresh BC blueberries.</p>
-                  <a href="#" class="btn btn-outline-primary">VIEW PRODUCT</a>
+                  <div class="carousel-content">
+                     <h3>Featured</h3>
+                     <h2>maple blueberry</h2>
+                     <p>The ultimate breakfast sausage. %100 maple syrup with allspice and marjoram, and of course, lots
+                        of
+                        fresh BC blueberries.</p>
+                     <a href="#" class="btn btn-outline-primary">VIEW PRODUCT</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="carousel-item">
+         <img src="<?php echo get_template_directory_uri(); ?>/images/featured-thumbnail.jpg" alt="featured-product">
+         <div class="container">
+            <div class="row justify-content-end">
+               <div class="col-md-6">
+                  <div class="carousel-content">
+                     <h3>Featured</h3>
+                     <h2>maple blueberry</h2>
+                     <p>The ultimate breakfast sausage. %100 maple syrup with allspice and marjoram, and of course, lots
+                        of
+                        fresh BC blueberries.</p>
+                     <a href="#" class="btn btn-outline-primary">VIEW PRODUCT</a>
+                  </div>
                </div>
             </div>
          </div>
       </div>
    </div>
    <a class="carousel-control-prev" href="#featured-products" role="button" data-slide="prev">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.813 29.313">
-         <path d="M-2724.775,1325.6l-16.407-13.3,16.407-13.2" stroke="#a46432" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2" stroke-dasharray="1 4" />
+      <svg xmlns="http://www.w3.org/2000/svg" width="18.813" height="29.313" viewBox="0 0 18.813 29.313">
+         <path d="M-2724.775,1325.6l-16.407-13.3,16.407-13.2" transform="translate(2742.182 -1297.695)"
+            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke-dasharray="1 4" />
       </svg>
       <span class="sr-only">Previous</span>
    </a>
    <a class="carousel-control-next" href="#featured-products" role="button" data-slide="next">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.813 29.313">
-         <path d="M-2724.775,1325.6l-16.407-13.3,16.407-13.2" stroke="#a46432" stroke-linecap="round"
-            stroke-linejoin="round" stroke-width="2" stroke-dasharray="1 4" />
+      <svg xmlns="http://www.w3.org/2000/svg" width="18.813" height="29.313" viewBox="0 0 18.813 29.313">
+         <path d="M-2724.775,1325.6l-16.407-13.3,16.407-13.2" transform="translate(-2723.369 1327.008) rotate(180)"
+            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke-dasharray="1 4" />
       </svg>
       <span class="sr-only">Next</span>
    </a>
 </section>
+
+<?php do_action('woocommerce_before_main_content'); ?>
+<section class="py-6">
+   <div class="container">
+      <div class="row">
+         <div class="col-12 pb-5">
+            <?php wc_get_template_part('loop', 'cats'); ?>
+         </div>
+         <div class="col-12">
+            <?php if (woocommerce_product_loop()) {
+              do_action('woocommerce_before_shop_loop');
+              woocommerce_product_loop_start();
+
+              if (wc_get_loop_prop('total')) {
+                while (have_posts()) {
+                  the_post();
+                  do_action('woocommerce_shop_loop');
+                  wc_get_template_part('content', 'product');
+                }
+              }
+
+              woocommerce_product_loop_end();
+              do_action('woocommerce_after_shop_loop');
+            } else {
+              do_action('woocommerce_no_products_found');
+            } ?>
+         </div>
+      </div>
+   </div>
+</section>
 <?php
-if (woocommerce_product_loop()) {
-  /**
-   * Hook: woocommerce_before_shop_loop.
-   *
-   * @hooked woocommerce_output_all_notices - 10
-   * @hooked woocommerce_result_count - 20
-   * @hooked woocommerce_catalog_ordering - 30
-   */
-  do_action('woocommerce_before_shop_loop');
-
-  woocommerce_product_loop_start();
-
-  if (wc_get_loop_prop('total')) {
-    while (have_posts()) {
-      the_post();
-
-      /**
-       * Hook: woocommerce_shop_loop.
-       */
-      do_action('woocommerce_shop_loop');
-
-      wc_get_template_part('content', 'product');
-    }
-  }
-
-  woocommerce_product_loop_end();
-
-  /**
-   * Hook: woocommerce_after_shop_loop.
-   *
-   * @hooked woocommerce_pagination - 10
-   */
-  do_action('woocommerce_after_shop_loop');
-} else {
-  /**
-   * Hook: woocommerce_no_products_found.
-   *
-   * @hooked wc_no_products_found - 10
-   */
-  do_action('woocommerce_no_products_found');
-}
-
 /**
  * Hook: woocommerce_after_main_content.
  *
  * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
 do_action('woocommerce_after_main_content');
-
 get_footer('shop');
---
+++ wp-content/themes/haussausageco/woocommerce/loop/loop-cats.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying the category lists in products category pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package haussausageco
+ */
+?>
+
+<ul>
+   <li><a href="#">Pork</a></li>
+   <li><a href="#">Specialities</a></li>
+   <li><a href="#">Chicken</a></li>
+</ul>
