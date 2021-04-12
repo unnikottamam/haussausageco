802efeba4c22a00a47b8aae91fabe277a3a97726
Unnikrishnan <unnikottamam@gmail.com>
2021-04-11 23:01:30 -0700

    updated the single page templates and cart page into the theme, needs to work on the styling of single page sections, taking break

--- wp-content/themes/haussausageco/woocommerce/single-product.php
+++ wp-content/themes/haussausageco/woocommerce/single-product.php
@@ -22,24 +22,11 @@ if (!defined('ABSPATH')) {
 get_header('shop');
 get_template_part('template-parts/top', 'banner');
 
-/**
- * woocommerce_before_main_content hook.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- */
 do_action('woocommerce_before_main_content');
-
 while (have_posts()) {
   the_post();
   wc_get_template_part('content', 'single-product');
 }
-
-/** // end of the loop.
- * woocommerce_after_main_content hook.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
- */
 do_action('woocommerce_after_main_content');
 
 get_footer('shop');
